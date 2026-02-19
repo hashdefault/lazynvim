877e5ab6ff4cea0200d12f18a0a5aa2c57fd3c5f
Lucas G. Bueno <lucasgbueno@proton.me>
2026-02-19 14:11:51 -0300

    add eldritch theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,14 +13,12 @@ return {
   --  },
   --},
   {
-    "sainnhe/everforest",
+    "eldritch-theme/eldritch.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.g.everforest_background = "hard"
-      vim.g.everforest_ui_contrast = "high"
-      -- vim.g.everforest_transparent_background = 1
-      vim.cmd.colorscheme("everforest")
-    end,
+    opts = {
+      transparent = true,
+      terminal_colors = true,
+    },
   },
 }
